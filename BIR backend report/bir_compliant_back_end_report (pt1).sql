c07992003652f0838fdbe1f39b3c213caa127f5d
John Paul Del Mundo <81340844+jpnotsodev@users.noreply.github.com>
2025-08-12 07:35:23 +0800

    added comments for better readability

--- BIR backend report/bir_compliant_back_end_report (pt1).sql
+++ BIR backend report/bir_compliant_back_end_report (pt1).sql
@@ -1,19 +1,25 @@
+USE backupdb; -- Default schema 
+
+-- Drop temp table if it already exists within selected schema
 DROP TABLE IF EXISTS sales_summary ; 
 
+-- Create temporary table
 CREATE TABLE sales_summary (trans_date DATE, beginning_or_no INT, 
 			ending_or_no INT, gross_sales_amt DECIMAL(20, 2), `returns` DECIMAL(20, 2),
 			discount DECIMAL(20, 2), terminalno INT, userid VARCHAR(20));
 			
+-- Populate temp table with data from epos transaction tables
 INSERT INTO sales_summary 
-
-SELECT CONVERT(h.`ERDAT`, DATE) AS `trans_date`, CONVERT(RIGHT(MIN(h.`ORDNUM`), 10), SIGNED) AS `beginning_or_no`, CONVERT(RIGHT(MAX(h.`ORDNUM`), 10), SIGNED) AS `ending_or_no` 
-	,SUM(d.`QTYORDERED`*d.`KBETR`) AS `gross_sales_amt`, 0, SUM(d.`QTYORDERED`*d.`KBETR`*d.`DISCPER`/100) AS `discount`, h.TERMINALNO, h.USERID
+-- The following query gets the min (beginning) and max (ending) order number per each day.
+-- Computes the gross sales amount, discount, returns etc.
+SELECT CONVERT(h.`ERDAT`, DATE) AS `trans_date`, CONVERT(RIGHT(MIN(h.`ORDNUM`), 10), SIGNED) AS `beginning_or_no`
+	,CONVERT(RIGHT(MAX(h.`ORDNUM`), 10), SIGNED) AS `ending_or_no` 
+	,SUM(d.`QTYORDERED`*d.`KBETR`) AS `gross_sales_amt`, 0
+	,SUM(d.`QTYORDERED`*d.`KBETR`*d.`DISCPER`/100) AS `discount`, h.TERMINALNO, h.USERID
 FROM zsd_eposordh h
 INNER JOIN zsd_eposordd d ON h.`MANDT` = d.`MANDT`
 			AND h.`ORDNUM` = d.`ORDNUM`
--- left join (select MANDT, ORDNUM, RET `returns` from zsd_eposcrdh rh GROUP BY ERDAT) ret ON h.MANDT = ret.MANDT
--- 										AND h.ORDNUM = ret.ORDNUM
-WHERE TERMINALNO = 3
+WHERE TERMINALNO = 3 -- Replace this value by the terminal number used by the last z-reading submitted by the operations dept. to BIR
 GROUP BY h.`ERDAT` ;
 
 DROP TABLE IF EXISTS sales_summary_w_ending_bal ;
@@ -26,13 +32,10 @@ INSERT INTO sales_summary_w_ending_bal
 SELECT s2.`trans_date`, s2.`beginning_or_no`, s2.`ending_or_no`
 	,s2.`gross_sales_amt`, s2.`returns`, s2.`discount`
 	, s1.`gross_sales_amt`, s2.TERMINALNO, s2.USERID
--- s1.`trans_date`, s1.`beginning_or_no`, s1.`ending_or_no`
--- 	,s1.`gross_sales_amt`, IFNULL(s2.`gross_sales_amt`, 0)+s1.`gross_sales_amt` as gross_ending_bal
--- 	,IFNULL(s2.`gross_sales_amt`, 0) as gross_beginning_bal
 FROM sales_summary s1
 RIGHT JOIN sales_summary s2 
 ON s1.ending_or_no+1 = s2.beginning_or_no;
--- where s2.trans_date >= '2014-03-01';
+WHERE s2.trans_date BETWEEN '2014-03-01' AND '2014-03-01'; -- Adjust the date if needed
 			
 ALTER TABLE sales_summary_w_ending_bal ADD COLUMN sales_beginning_bal DECIMAL(20, 2) NULL ;
 ALTER TABLE sales_summary_w_ending_bal ADD COLUMN sales_ending_bal DECIMAL(20, 2) NULL ;
@@ -41,16 +44,15 @@ CREATE INDEX UNIQUE_trans_date ON sales_summary (trans_date) ;
 CREATE INDEX INDEX_beg_or_no ON sales_summary (beginning_or_no) ;
 CREATE INDEX INDEX_ending_or_no ON sales_summary (ending_or_no) ;
 
--- select * from sales_summary_w_ending_bal;
-
 DELIMITER $$
- 
+
 DROP TABLE IF EXISTS `tbl_returns` $$
 
 CREATE TABLE `tbl_returns` (
     trans_date DATE, return_amt DECIMAL(20, 2)
 ) $$
 
+-- Get return amounts
 INSERT INTO `tbl_returns` 
 SELECT CONVERT(h.ERDAT, DATE) AS `trans_date`, 
 	SUM(RET)
@@ -58,13 +60,12 @@ FROM zsd_eposcrdh h
 WHERE TERMINALNO = 3
 GROUP BY h.ERDAT $$
 
+-- Update placeholders with values from tblrefunds
 UPDATE sales_summary_w_ending_bal
 INNER JOIN tbl_returns ON sales_summary_w_ending_bal.`trans_date` = tbl_returns.`trans_date`
 SET sales_summary_w_ending_bal.`returns` = tbl_returns.`return_amt` $$
  
--- select * from sales_summary_w_ending_bal $$
-
-
+-- Gets total net sales
 SELECT SUM(gross_sales_amt-`discount`-`returns`) FROM sales_summary_w_ending_bal WHERE TERMINALNO = 3
 
 SELECT * fro m
